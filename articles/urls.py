bc08c203ed56e90f85663c83e0848726356bd0d2
Mad <killerfeed86@gmail.com>
2025-02-04 07:33:28 +0600

    D

--- articles/urls.py
+++ articles/urls.py
@@ -1,15 +1,8 @@
 from django.urls import path
-from django.shortcuts import render 
-from . import views
-from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
-def home(request):
-    return render(request, 'index.html')
 urlpatterns = [
-    path('', views.home, name='home'),  # Главная страница
-    path("LBLB/index.html", home),  # Главная страница
     path('', views.article_list, name='article_list'),
     path('article/<int:pk>/', views.article_detail, name='article_detail'),  # Новый URL для подробностей
 ]
